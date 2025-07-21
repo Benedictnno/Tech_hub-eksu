613ff94223a1718596d7388abfc33a0e98832d0a
BEN <benedictnnaoma0@gmail..com>
2025-07-21 12:28:26 +0100

    qr code

--- project/components/QrCodeMaker.vue
+++ project/components/QrCodeMaker.vue
@@ -1,14 +1,17 @@
+
 <template>
-  <div class="w-fit">
-    <canvas ref="qrCanvas" />
-  </div>
+  <!-- QR code will be rendered here -->
+  <canvas ref="qrCanvas"></canvas>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { ref, watch, nextTick } from 'vue'
 import QRCode from 'qrcode'
-const config = useRuntimeConfig();
 
+// Runtime config (Nuxt 3)
+const config = useRuntimeConfig()
+
+// Props
 const props = defineProps({
   userId: {
     type: String,
@@ -16,14 +19,42 @@ const props = defineProps({
   }
 })
 
-console.log(props.userId);
-
+// Ref for canvas
 const qrCanvas = ref(null)
 
-onMounted(() => {
+// Function to generate QR code
+function generateQRCode() {
   const url = `${config.public.URL}/user/${props.userId}`
+  // console.log("Generating QR for:", url)
+
+  if (!qrCanvas.value) {
+    console.error("Canvas not mounted yet.")
+    return
+  }
+
   QRCode.toCanvas(qrCanvas.value, url, { width: 200 }, (error) => {
-    if (error) console.error(error)
+    if (error) {
+      console.error("QR generation failed:", error)
+    } else {
+      console.log("QR code generated for:", props.userId)
+    }
   })
-})
+}
+
+// Watch for userId and canvas availability
+watch(
+  () => props.userId,
+  async (newVal) => {
+    if (!newVal) return
+
+    await nextTick() // wait for DOM to render canvas
+    generateQRCode()
+  },
+  { immediate: true }
+)
 </script>
+
+
+
+
+
